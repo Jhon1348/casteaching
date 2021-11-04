fe3448766c88d091dbb20be196e6375144169aa9
Jhon <jmoreno1@iesebre.com>
2021-11-04 17:14:51 +0100

    Primer test VideoTest Green

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,25 @@
 <?php
 
+use App\Http\Controllers\VideosController;
+use App\Models\Video;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/videos/{id}', [VideosController::class, 'show']);
+
+
+//Route::get('/videos/1', function () {
+////    return 'Ubuntu 101 | Here description | December 13';
+//    $video = Video::find(1);
+////    dd($video->title);
+////    $video = new stdClass();
+////    $video->title = 'Ubuntu 101';
+////    $video->description = 'Here description';
+////    $video->published_at ='December 13';
+//    return view('videos.show',[
+//        'video' =>$video
+//    ]); //CRED -> RETRIEVE -> nomes un video
+//});
---
+++ tests/Feature/Videos/VideoTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature\Videos;
+
+use App\Models\Video;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class VideoTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * @test
+     */
+    public function users_can_view_videos()
+    {
+        //FASE 1 -> preparació
+        $video= Video::create([
+            'title' => 'Ubuntu 101',
+            'description' => '# Here description',
+            'url' => 'https://youtu.be/w8j07_DBl_I',
+            'published_at' => Carbon::parse('December 13, 2020 8:00pm'),
+            'previous' => null,
+            'next' => null,
+            'series_id' => 1
+        ]);
+
+        $video2= Video::create([
+            'title' => 'Ubuntu 101',
+            'description' => '# Here description',
+            'url' => 'https://youtu.be/w8j07_DBl_I',
+            'published_at' => Carbon::parse('December 13, 2020 8:00pm'),
+            'previous' => null,
+            'next' => null,
+            'series_id' => 1
+        ]);
+        //FASE 2 -> execució
+
+        $response = $this->get('/videos/' . $video2->id);
+
+        $response->assertStatus(200);
+        $response->assertSee('Ubuntu 101');
+        $response->assertSee('Here description');
+        $response->assertSee('December 13');
+        //FASE 3 -> assertions
+    }
+}
