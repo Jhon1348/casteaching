064893f725d5cd8ed891eeef10fda690fd6014eb
Jhon <jmoreno1@iesebre.com>
2021-11-11 17:02:27 +0100

    Resultat video 107

--- config/casteaching.php
+++ config/casteaching.php
@@ -2,6 +2,9 @@
 
 return[
  'default_user' =>[
+     'name'=> env('DEFAULT_USER_NAME','Jhon'),
+     'email'=> env('DEFAULT_USER_EMAIL','jhon@gmail.com'),
      'password'=> env('DEFAULT_USER_PASSWORD','12345678')
+
  ]
 ];
