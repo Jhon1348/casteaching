af33c54f4f3de7dda20639f8bf523a230d90b438
Jhon <jmoreno1@iesebre.com>
2021-11-11 21:08:15 +0100

    Resultat video 110

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -1,17 +1,22 @@
 <x-casteaching-layout>
-    <div class="flex flex-col h-screen">
-        <iframe
-            class="md:p-3 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:py-10 h-4/5"
-            src="https://www.youtube.com/embed/btGr3mPK1dU"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen></iframe>
-        <div class="p-4 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:py-20">
-            <h2>{{$video->title}}</h2>
-        </div>
-        <div class="p-4 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:py-20">
-            {{$video->description}}
+    <div class="flex flex-col space-x-4 space-y-4 lg:space-x-4
+    lg:space-y-4 xl:space-x-10 xl:space-y-5 2xl:space-x-20 2xl:space-y-10 items-center ">
+            <iframe
+                class="md:p-3 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:py-10 h-4/5 w-full md:px-6 xl:px-15 xl:py-5 2xl:px-20 2xl:py-10"
+                style="height: 75vh"
+                src="https://www.youtube.com/embed/btGr3mPK1dU"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen>
+            </iframe>
+
+        <h2 class="text-gray-900 uppercase font-bold text-2xl tracking-tighter">
+            {{$video->title}}
+        </h2>
+
+        <div class="prose-sm md:prose lg:prose-xl 2xl:prose-2xl">
+            {!! Str::markdown($video->description) !!}
         </div>
     </div>
 </x-casteaching-layout>
