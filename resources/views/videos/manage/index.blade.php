23be3a422fc8bf177593a026a360a141eb8c65d7
Jhon <jmoreno1@iesebre.com>
2021-11-29 17:39:33 +0100

    segona part creació d'un formulari HTML

--- resources/views/videos/manage/index.blade.php
+++ resources/views/videos/manage/index.blade.php
@@ -53,7 +53,7 @@
                   <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
                     http://
                   </span>
-                                                        <input type="text" name="url" id="url" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="youtube.com/">
+                                                        <input type="text" name="url" id="url" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="casteaching.jhonmoreno.codes/">
                                                     </div>
                                                 </div>
                                             </div>
