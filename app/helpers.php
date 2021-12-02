243169d578d6ee691365304470560111917b1c2e
Jhon <jmoreno1@iesebre.com>
2021-12-02 17:53:04 +0100

    Completem el CRUD amb la edició

--- app/helpers.php
+++ app/helpers.php
@@ -60,9 +60,13 @@ if(! function_exists('create_video_manager_user')) {
         Permission::create(['name' => 'videos_manage_index']);
         Permission::create(['name' => 'videos_manage_create']);
         Permission::create(['name' => 'videos_manage_destroy']);
+        Permission::create(['name' => 'videos_manage_edit']);
+        Permission::create(['name' => 'videos_manage_update']);
         $user->givePermissionTo('videos_manage_index');
         $user->givePermissionTo('videos_manage_create');
         $user->givePermissionTo('videos_manage_destroy');
+        $user->givePermissionTo('videos_manage_edit');
+        $user->givePermissionTo('videos_manage_update');
         add_personal_team($user);
         return $user;
     }
@@ -116,7 +120,10 @@ if(! function_exists('create_permissions')) {
     function create_permissions (){
         Permission::firstOrCreate(['name' => 'videos_manage_index']);
         Permission::firstOrCreate(['name' => 'videos_manage_create']);
+        Permission::firstOrCreate(['name' => 'videos_manage_store']);
         Permission::firstOrCreate(['name' => 'videos_manage_destroy']);
+        Permission::firstOrCreate(['name' => 'videos_manage_edit']);
+        Permission::firstOrCreate(['name' => 'videos_manage_update']);
     }
 }
 
