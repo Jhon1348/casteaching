8432b36b0786fccb988b748d6142aa8c9ca15925
Jhon <jmoreno1@iesebre.com>
2022-01-27 16:34:31 +0100

    Laravel Sanctum Proporcionant tokens als usuaris amb TDD

--- resources/js/components/VideosList.vue
+++ resources/js/components/VideosList.vue
@@ -88,7 +88,7 @@ export default
     },
     methods: {
         async getVideos() {
-            this.videos = await window.casteaching({baseUrl:'https://casteaching.test/api'}).videos()
+            this.videos = await window.casteaching({baseUrl:'https://casteaching.test/api'})
         },
         async refresh() {
             this.getVideos()
