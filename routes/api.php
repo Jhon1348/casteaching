dab264cd6af537a44c65c3dcc4830ecdecc2a082
Jhon <jmoreno1@iesebre.com>
2022-02-01 17:24:38 +0100

    Recopilació, relacions 1 a n inverses i adaptació del codi existent de videos

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-sleep(2);
+//sleep(2);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
